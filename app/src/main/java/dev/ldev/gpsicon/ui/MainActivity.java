626edbb98f8b7636272fdb6c0c64813361c5e1f3
Limizin <none@none>
2016-07-08 01:03:51 +0300

    start icon service after installation id gps is on

--- app/src/main/java/dev/ldev/gpsicon/ui/MainActivity.java
+++ app/src/main/java/dev/ldev/gpsicon/ui/MainActivity.java
@@ -1,18 +1,26 @@
 package dev.ldev.gpsicon.ui;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import dev.ldev.gpsicon.C;
 import dev.ldev.gpsicon.R;
 import dev.ldev.gpsicon.notify.NotifyIconProviderDirector;
 import dev.ldev.gpsicon.notify.NotifyIconTypes;
+import dev.ldev.gpsicon.services.GpsObserveService;
 
 public class MainActivity extends Activity {
+
+    private final static String TAG="main";
 
     private RadioButton _blinkIconRadio;
     private RadioButton _buIconRadio;
@@ -46,6 +54,21 @@ public class MainActivity extends Activity {
                 switchNotifyIcon(NotifyIconTypes.BU);
             }
         });
+
+        startServiceIfNeed();
+    }
+
+    private void startServiceIfNeed() {
+        try {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                startService(new Intent(this, GpsObserveService.class));
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "start gps icon service error", e);
+            Toast.makeText(this, e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
     }
 
     private void switchNotifyIcon(String targetIcon) {
